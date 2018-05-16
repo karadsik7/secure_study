9027827fa32b99b3ec71b6ad451b37013a1b3409
026 <026@192.168.0.31>
2018-05-16 17:35:18 +0900

    로그인 검증

---
+++ ch04_LoginAttempt/src/vo/PhotoVo.java
@@ -0,0 +1,26 @@
+package vo;
+
+public class PhotoVo {
+	private int idx;
+	private String description;
+	private String url;
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+}
